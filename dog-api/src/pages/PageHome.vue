112e24697a8d73d5ff5f34c60a1deb67b443cb07
pc4ucode <euler.amorim@hotmail.com>
2021-03-26 19:32:05 -0300

    projeto inicial

---
+++ dog-api/src/pages/PageHome.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>        
+        <ListPage/>
+    </div>
+</template>
+
+<script>
+
+import ListPage from '../components/ListPage';
+
+export default {
+    name: 'PageHome',
+
+    components: {
+        ListPage
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
